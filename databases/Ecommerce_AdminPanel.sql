7b6bc538a63083a7ec2972d2f9d8d5a4742c0037
vj2001 <vishaljain63957@gmail.com>
2022-02-25 12:25:25 +0530

    functionality added

--- databases/Ecommerce_AdminPanel.sql
+++ databases/Ecommerce_AdminPanel.sql
@@ -4,28 +4,33 @@
 )*/
 
 create table users(
-   userid INT PRIMARY KEY SERIAL,
-   username varchar(50),
-   --roleid INT REFERENCES roles(roleid) 
+   userid SERIAL PRIMARY KEY,
+   username varchar(50)
 )
+
 create table categories(
-    catid INT PRIMARY KEY SERIAL,
+    catid SERIAL PRIMARY KEY,
 	catname varchar(30) UNIQUE
 )
 create table products(
-	 productid INT PRIMARY KEY SERIAL,
-	 productname varchar(50) UNIQUE,
+	 productid SERIAL PRIMARY KEY,
+	 productname varchar(50),
 	 catid INT references categories(catid),
 	 price INT,
-	 prodcount INT,
+	 prodcount INT
 )
+-- ALTER table products ADD COLUMN productname varchar(50);
+-- alter table products alter price type decimal;
+-- delete from products 
+
 create table orders(
-      orderid INT PRIMARY KEY SERIAL,
+      orderid SERIAL PRIMARY KEY,
 	  recid INT REFERENCES users(userid),
 	  prodid INT references products(productid),
 	  ordertime TIMESTAMP NOT NULL,
 	  totalcost INT
 )
+alter table orders alter totalcost type decimal;
 
 CREATE OR REPLACE FUNCTION increase_the_products()
 
@@ -34,19 +39,20 @@ CREATE OR REPLACE FUNCTION increase_the_products()
 	as
 	$$
 		 BEGIN
-		             IF EXISTS(select * from products where productname=NEW.productname) then
+		             IF (select COUNT(*) from products where productname=NEW.productname)>1 then
 					 UPDATE products SET prodcount= prodcount+1 where productname = NEW.productname;
+					 DELETE from products where productid= NEW.productid;
 					 ELSE
-					 INSERT INTO products(productname,catid,price,prodcount)
-					                      values(NEW.productname,NEW.catid,NEW.price,1)
+					 UPDATE products SET prodcount= 1 where productid = NEW.productid;
 					 END IF;
-					 
+					 RETURN NEW;
 		 END;
 	$$
 
+
 -- creating trigger to increase count of specific product
 CREATE TRIGGER increase_products_count    
- INSTEAD OF INSERT
+ AFTER INSERT
  ON products
  FOR EACH ROW
      EXECUTE PROCEDURE increase_the_products();
@@ -59,28 +65,45 @@ CREATE OR REPLACE FUNCTION decrease_the_products()
 	LANGUAGE PLPGSQL
 	as
 	$$
-		 BEGIN
+		 BEGIN        
 		              UPDATE products SET prodcount= prodcount-1 where productid = NEW.prodid;
-				      UPDATE orders set totalcost= (select price from products where productid=NEW.productid); 
+				      UPDATE orders set totalcost= (select price from products where productid=NEW.prodid)
+					  where orderid=NEW.orderid;
+					  RETURN NEW;
 		 END;
 	$$
 
--- creating trigger to increase count of specific product
+-- creating trigger to decrease count of specific product
 CREATE TRIGGER decrease_products_count    
- BEFORE INSERT
+ AFTER INSERT
  ON orders
  FOR EACH ROW
-     EXECUTE PROCEDURE decrease_the_products()ategories	;
+     EXECUTE PROCEDURE decrease_the_products();
 
 
 -- creating complete view
 
-CREATE VIEW order_details
+CREATE OR REPLACE VIEW order_details
 as
-   select o.orderid,o.ordertime,o.totalcost,p.productname,p.prodcount,c.catname from orders o
+   select o.orderid,o.ordertime,o.totalcost,p.productname,p.prodcount,c.catname,u.username from orders o
    INNER JOIN products p
    ON o.prodid = p.productid
    INNER JOIN users u
    ON o.recid = u.userid
    INNER JOIN categories c
    ON p.catid = c.catid
+   
+ select * from order_details  
+   
+ insert into users(username) values ('Vishal'),('Tom'),('Dhanush');
+ select * from users;
+ insert into categories(catname) values ('Electronics'),('Grocery');
+ select * from categories;
+ insert into products(productname,catid,price) values('Headphones',1,850),('Noodles',2,30),('Earphones',1,230);
+ select * from products
+ insert into products(productname,catid,price) values('Headphones',1,850)
+ 
+ insert into orders(recid,prodid,ordertime) values(1,13,now()),(2,12,now())
+ select * from orders
+  insert into orders(recid,prodid,ordertime) values(1,11,now())
+  insert into products(productname,catid,price) values('Earphones',1,230),('Cornflakes',2,432.5);
